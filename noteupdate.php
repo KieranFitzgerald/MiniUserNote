0ad803928637acf704d59e016d78814d1bc9aab4
luntdotp <peter@snapki.net>
2012-12-23 10:24:37 +0000

    Better JS and PHP Minified

--- noteupdate.php
+++ noteupdate.php
@@ -1,6 +1,2 @@
-<?php
-session_start(); 
- $note = strip_tags($_POST['notearea']);
- $_SESSION['note'] = $note;
-?>
-<script>alert('Note Updated'); history.go(-1); </script>
+<?php session_start(); $note = strip_tags($_POST['notearea']); $_SESSION['note'] = $note; ?>
+<script type="application/javascript">function done(){ alert('Note Updated'); history.go(-1); } done()</script>
