247c0bcbc85d92307af01afdebf82836a0ad4a76
luntdotp <peter@snapki.net>
2012-12-23 14:17:11 +0000

    Wow I am bad a multiple selectors at the moment

--- index.php
+++ index.php
@@ -2,39 +2,9 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-
+<link rel="stylesheet" type="text/css" href="note.css">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>Note - Snapki.net</title>
-<style>
-#note.h1 {
-	font-size: 18px;
-	line-height: 20px;	
-}
-
-#note {
-	width: 200px;
-	height: 200px;
-	background-color: rgb(255, 255, 153);
-	border-radius: 7px;
-	-moz-border-radius: 7px;
-	paddin-left: 5px;
-}
-
-.notearea {
-	width: inherit;
-	margin-left: 5px;
-	margin-right: 5px;
-	background-color: rgb(255, 255, 153);	
-	border: #999 2px solid;
-}
-
-.notesub {
-	width: inherit;
-	margin-left: 5px;
-	margin-right: 5px;	
-}
-
-</style>
 </head>
 
 <body>
@@ -44,7 +14,7 @@
 <div id="notetxt">
 <form method="post" action="noteupdate.php">
 <textarea name="notearea"><?php echo $_SESSION['note']; ?></textarea><br />
-<input type="submit" name="notesub" value="Save for now..." />
+<input type="submit" name="notesub" id="notesub" value="Save for now..." />
 </form>
 </div>
 </div>
