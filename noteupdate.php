8f192730bc29c99b3a6d76ecdfb32358c5ec4bc9
Peter Lunt <peter@snapki.net>
2013-01-09 13:51:00 +0000

    Semicolon...

--- noteupdate.php
+++ noteupdate.php
@@ -1,2 +1,2 @@
 <?php session_start(); $note = strip_tags($_POST['notearea']); $_SESSION['note'] = $note; ?>
-<script type="application/javascript">function done(){ alert('Note Updated'); history.go(-1); } done()</script>
+<script type="application/javascript">function done() { alert('Note Updated'); history.go(-1); } done();</script>
