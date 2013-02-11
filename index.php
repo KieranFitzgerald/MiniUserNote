b8c13bc7149f6470e2375cc7fde36ae5e0d4b799
luntdotp <peter@snapki.net>
2013-02-11 18:23:15 +0000

    Styled the things.

--- index.php
+++ index.php
@@ -14,9 +14,9 @@
 <h3>My Note...</h3>
 <div id="notetxt">
 <form method="post" action="noteupdate.php">
-<textarea name="notearea"><?php echo $_SESSION['note']; ?></textarea><br />
-<input type="submit" name="notesub" id="notesub" value="Save for now..." />
-</form>
+<textarea name="notearea" class="notecontent"><?php echo $_SESSION['note']; ?></textarea><br />
+<input type="submit" name="notesub" class="notesub" id="notesub" value="Save for now..." />
+</form><div class="info"><strong>i</strong><br />Information about this can go here.</div>
 </div>
 </div>
 
